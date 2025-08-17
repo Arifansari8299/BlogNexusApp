a52f97c7023efa55a48b109733cb93fa1ba21c3a
Arif Ansari <arifkmclu@gmail.com>
2025-08-17 18:49:59 +0530

    created userSchema

---
+++ Backend/models/Users/User.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose')
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        required:true,
+        enum:["user","admin"],
+        default:"user"
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    lastlogin:{
+        type:Date,
+        default:Date.now
+    },
+    isVerified:{
+        type:Boolean,
+        default:false,
+    },
+    accountLevel:{
+        type:String,
+        enum:["bronze","silver","gold"],
+        default:"bronze"
+    },
+    profilePicture:{
+      type:String,
+      default:""
+    },
+    coverImage:{
+        type:String,
+        default:""
+      },
+      bio:{
+        type:String,
+      },
+      location:{
+        type:String
+      },
+      notificationType:{
+       email:{
+        type:String,
+        required:true
+       }
+      },
+      gender:{
+        type:String,
+        enum:["male","female","prefer not to say","non-binary"]
+      },
+    //   other properties will deal with relationship
+});
