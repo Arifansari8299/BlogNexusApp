fc29c51abe9f393306bc11b1f5e63f97c352b588
Arif Ansari <arifkmclu@gmail.com>
2025-08-18 18:20:38 +0530

    created Category Model

---
+++ Backend/models/Categories/Category.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const CategorySchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    author:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    shares:{
+        type:Number,
+        default:0
+    },
+    post:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Post"
+    },
+
+},{timestamps:true});
+
+//! convert schema to model
+const Category = mongoose.model("Category",CategorySchema);
+
+module.exports = Category;
