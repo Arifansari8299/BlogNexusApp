999754ad5f4b1f18480783141fafa3a2b4fafe5e
Arif Ansari <arifkmclu@gmail.com>
2025-08-18 18:27:28 +0530

    created Comment Model

---
+++ Backend/models/Comments/Comment.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+const CommentSchema = new mongoose.Schema({
+    message:{
+        type:String,
+        required:true
+    },
+    author:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    postId: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Post",
+        required:true
+    },
+
+},{timestamps:true});
+ const Comment = mongoose.model("Comment",CommentSchema);
+ module.exports = Comment
